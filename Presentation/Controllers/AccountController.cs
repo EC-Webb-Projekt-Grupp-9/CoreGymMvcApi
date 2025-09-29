fd111d50cb23f804aaf5cc3283d9ce37f62ec221
rdunder <r.dunder@gmail.com>
2025-09-29 15:40:27 +0200

    Created AccountController with endpoint to register a user

---
+++ Presentation/Controllers/AccountController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Service.Dtos;
+using Service.Interfaces;
+using Service.Models;
+
+namespace Presentation.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class AccountController(IAccountService accountService) : ControllerBase
+{
+    private readonly IAccountService _accountService = accountService;
+
+    [HttpPost]
+    public async Task<IActionResult> Register(UserRegisterDto dto)
+    {
+        
+        if (!ModelState.IsValid)
+        {
+            return BadRequest();
+            
+        }
+
+        var result = await _accountService.Register(dto);
+        return result ? Ok() : Problem("Could not create user");
+    }
+}
